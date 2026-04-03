2ec0b9bc500cb58966327457be0c487e8ab94763
noa.la <noa.la@lycee-de-beauvoir.education>
2026-04-03 09:46:13 +0200

    affichage de l'âge

--- landsmann_force_manege.py
+++ landsmann_force_manege.py
@@ -9,4 +9,8 @@ label=Label(fenetre, text="Manege du parc - Force 1 / Force 2",fg="navy",font="1
 label.pack()
 label=Label(fenetre, text="Entrez votre âge et votre taille pour savoir à quelle force vous pouvez faire le manège.",wraplength=400,pady=10)
 label.pack()
+label=Label(fenetre, text="Entrez votre âge : ")
+label.pack()
+age=Entry(fenetre, width=10)
+age.pack()
 fenetre.mainloop()
